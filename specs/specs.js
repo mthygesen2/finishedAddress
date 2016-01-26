616d5d6588abdec050fd8b153673222b1a79f0c5
Cassandra Culkins and Mao Thygesen <cassy.culkins@gmail.com>
2016-01-26 08:32:59 -0800

    {

---
+++ specs/specs.js
@@ -0,0 +1,20 @@
+describe('Contact', function() {
+  it("creates a new contact with the given properties", function () {
+    var testContact = new Contact("Rita", "Moreno");
+    expect(testContact.firstName).to.equal("Rita");
+    expect(testContact.lastName).to.equal("Moreno");
+    expect(testContact.addresses).to.eql([]);
+  });
+  it("adds the fullName method to all contacts", function() {
+    var testContact = new Contact("Sherlock", "Holmes");
+    expect(testContact.fullName()).to.equal("Sherlock Holmes");
+  });
+});
+describe('Address', function(){
+  it("create address for each contact", function(){
+    var testAddress = new Address("123 4th St", "Portland", "Oregon");
+    expect(testAddress.street).to.equal("123 4th St");
+    expect(testAddress.city).to.equal("Portland");
+    expect(testAddress.state).to.equal("Oregon");
+  });
+});
